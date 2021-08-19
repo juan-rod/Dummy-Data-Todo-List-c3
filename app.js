f932cf7dcc6d9d518889002858b40c3f5a9b5c92
juan-rod <juan.j.rodriguez2@gmail.com>
2021-08-18 19:51:52 -0500

    filter

--- app.js
+++ app.js
@@ -37,12 +37,43 @@
   
   const populateTodos = () => {
    console.log('arrayOfTodos:', arrayOfTodos)
+   let ol = document.querySelector('#todo-list');
+   console.log('ol:', ol)
+  //  let ol = document.getElementById('todo-list');
    for (let index = 0; index < arrayOfTodos.length; index++) {
-     const todoItem = arrayOfTodos[index];
-     console.log('todoItem.completed', todoItem.id, todoItem.completed)
-     // get ol element from dom
-     // create li
-     
+     const todoItemTitle = arrayOfTodos[index].title;
+    
+    // if (!arrayOfTodos[index].completed) {
+      let listItem = document.createElement('li');
+      listItem.append(todoItemTitle)
+      ol.append(listItem)
+    // }
      
    }
   }
+
+  // let userId = 4
+  const filterUserId = () => {
+    // grab input value
+    let userId = document.querySelector('input').value
+    console.log('userId',userId)
+    let isUserFour = arrayOfTodos.filter(todo => {
+      console.log('todo.userId',todo.userId)
+      return todo.userId == userId && todo.completed
+    })
+    console.log('isUserFour:', isUserFour)
+    // how to remove list items from html with javascript
+  }
+// Fetch the same data.
+// Store the data in a variable.
+// Add an input for the userID. This input should only take in a number from 1 - 10.
+// Add a button that when clicked will:
+// clear the previous todos from the view
+// and populate it with only todos with the userID that matches the number inputted.
+// then stores the currently filtered todos in a variable so that ...
+// You can create two more buttons that when clicked:
+
+// removes those todos from the view
+// and shows only todos that are either:
+// completed
+// not completed
